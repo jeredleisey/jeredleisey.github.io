cb81add06d56c12e3bb88a851f44d8c7a655468f
Jered Leisey <jered.leisey@gmail.com>
2022-08-09 16:00:31 -0400

    forgot to commit. oops.

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,4 @@ import MainPage from "./components/MainPage.vue";
   <MainPage />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,7 +1,97 @@
-<script setup lang="ts">
-import moment from "moment";
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div class="main">{{ moment() }}</div>
+  <section>
+    <nav>
+      <h4>developedbyjered</h4>
+      <a class="button">Sign Up</a>
+    </nav>
+    <div class="container">
+      <h2 class="main-title">hello. my name is jered.</h2>
+    </div>
+    <div class="boxes">
+      <div
+        class="image"
+        style="
+          background-image: url('https://www.belancio.com/foundation/wp-content/uploads/curology-mini.jpg');
+          width: 100%;
+          height: 100vh;
+        "
+      ></div>
+      <div
+        class="image"
+        style="
+          background-image: url('https://www.belancio.com/foundation/wp-content/uploads/tradesy-mini.jpg');
+          width: 100%;
+          height: 100vh;
+        "
+      ></div>
+      <div
+        class="image"
+        style="
+          background-image: url('https://www.belancio.com/foundation/wp-content/uploads/revel-seasonings-mini.jpg');
+          width: 100%;
+          height: 100vh;
+        "
+      ></div>
+      <div
+        class="image"
+        style="
+          background-image: url('https://www.belancio.com/foundation/wp-content/uploads/irvine-copmany-mini.jpg');
+          width: 100%;
+          height: 100vh;
+        "
+      ></div>
+      <div
+        class="image"
+        style="
+          background-image: url('https://www.belancio.com/foundation/wp-content/uploads/rightnow-mini.jpg');
+          width: 100%;
+          height: 100vh;
+        "
+      ></div>
+    </div>
+  </section>
 </template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
+}
+
+nav {
+  position: absolute;
+  width: 100%;
+  display: flex;
+  padding: 3rem 10rem;
+  justify-content: space-between;
+}
+
+.container {
+  height: 100vh;
+}
+
+.container h4 {
+  z-index: 0;
+}
+
+.main-title {
+  position: fixed;
+  top: 45%;
+  left: 45%;
+  font-size: 4rem;
+  transform: translate(-20%, -45%);
+}
+
+.boxes {
+  display: block;
+  background: rgb(24, 24, 24);
+  overflow: hidden;
+  position: absolute;
+  z-index: 2;
+  width: 100%;
+}
+</style>
