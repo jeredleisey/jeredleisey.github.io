4d830e53f50d745e1db9522f0cf39289da7755cb
Jered Leisey <jered@radon.com>
2022-08-11 12:36:17 -0400

    new hero animation

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,18 +1,17 @@
 <script setup lang="ts"></script>
 
 <template>
-  <video autoplay muted loop id="myVideo">
-    <source src="../assets/bouncy.mp4" type="video/mp4" />
-  </video>
-
-  <section>
+  <section id="nav">
     <nav>
       <h4>developedbyjered</h4>
     </nav>
-    <div class="container">
-      <h2 class="main-title">one day...</h2>
-    </div>
   </section>
+
+  <section id="hero">
+    <div id="text">jdl</div>
+  </section>
+
+  <section id="secondary"></section>
 </template>
 
 <style>
@@ -23,37 +22,53 @@
   font-family: 'Righteous', cursive;
 }
 
+body {
+  background: url('https://images.unsplash.com/photo-1485841938031-1bf81239b815');
+  background-size: 100%;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-attachment: fixed;
+}
+
 nav {
   position: fixed;
   width: 100%;
   display: flex;
   padding: 3rem 10rem;
   justify-content: space-between;
+  color: white;
 }
 
-.container {
+#hero {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#secondary {
+  height: 500vh;
+}
+
+#app {
   height: 100vh;
 }
 
-.container h4 {
-  z-index: 0;
-}
-
-.main-title {
-  position: fixed;
-  top: 55%;
-  left: 65%;
-  font-size: 4rem;
+#text {
+  background: url('https://i.imgur.com/l0ff65M.gif');
+  background-size: 100%;
+  background-position: center;
+  background-clip: text;
+  color: #00000000;
   text-transform: uppercase;
-  color: #fff;
-  z-index: 0;
-}
-
-#myVideo {
-  position: fixed;
-  top: 0;
-  left: 0;
-  min-width: 100%;
-  min-height: 100%;
+  -webkit-text-stroke: 1px #3f3f3f;
+  font-size: 30vw;
+  font-family: 'Righteous', cursive;
+  letter-spacing: 1px;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  text-align: center;
 }
 </style>
