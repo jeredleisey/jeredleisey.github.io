bc922bbc822c3376ccb65a7fc380c940517b12c7
Jered Leisey <jered.leisey@gmail.com>
2023-01-05 22:02:34 -0500

    Created Journal page; styling changes

---
+++ src/pages/JournalPage.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+import { loremIpsum } from 'lorem-ipsum';
+
+const route = useRoute();
+</script>
+
+<template>
+  <div class="blog-content">
+    <span class="title">
+      {{ route.params.articleTitle }}
+    </span>
+    <div class="content" v-for="i in 10" :id="'paragraph-' + i">
+      {{ loremIpsum({ count: 1, units: 'paragraphs' }) }}
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.blog-content {
+  margin: auto;
+  margin-top: 2em;
+  display: flex;
+  flex-direction: column;
+  max-width: 520px;
+  position: relative;
+}
+.title {
+  margin: auto;
+  font-family: kepler-std, serif;
+  font-weight: 700;
+  font-size: 3em;
+  white-space: nowrap;
+}
+.content {
+  margin-bottom: 1em;
+  font-family: 'kepler-std', serif;
+  font-weight: 400;
+  font-size: 1.3em;
+}
+p {
+  margin-bottom: 1em !important;
+}
+</style>
