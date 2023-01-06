b42331b19857e9cccd5727d79ed9c35e3c69e011
Jered Leisey <jered.leisey@gmail.com>
2023-01-05 22:10:40 -0500

    Added Footer component; styling changes

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import SiteHeader from './components/SiteHeader.vue';
+import SiteFooter from './components/SiteFooter.vue';
 </script>
 
 <template>
   <SiteHeader />
   <router-view></router-view>
+  <SiteFooter />
 </template>
 
 <style scoped></style>
--- src/pages/JournalPage.vue
+++ src/pages/JournalPage.vue
@@ -6,24 +6,30 @@ const route = useRoute();
 </script>
 
 <template>
-  <div class="blog-content">
-    <span class="title">
-      {{ route.params.articleTitle }}
-    </span>
-    <div class="content" v-for="i in 10" :id="'paragraph-' + i">
-      {{ loremIpsum({ count: 1, units: 'paragraphs' }) }}
+  <div class="blog-page">
+    <div class="blog-content">
+      <span class="title">
+        {{ route.params.articleTitle }}
+      </span>
+      <div class="content" v-for="i in 10" :id="'paragraph-' + i">
+        {{ loremIpsum({ count: 1, units: 'paragraphs' }) }}
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+.blog-page {
+  background-color: #fff;
+  position: relative;
+  z-index: 1;
+}
 .blog-content {
   margin: auto;
-  margin-top: 2em;
+  padding-top: 2em;
   display: flex;
   flex-direction: column;
   max-width: 520px;
-  position: relative;
 }
 .title {
   margin: auto;
