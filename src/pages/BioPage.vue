8eb607a656ec556afab72fecd20959d4a7f73d6f
Jered Leisey <jered.leisey@gmail.com>
2023-02-20 15:40:37 -0500

    style: update page layout to be mobile-friendly

--- src/pages/BioPage.vue
+++ src/pages/BioPage.vue
@@ -7,24 +7,21 @@ gsap.registerPlugin(DrawSVGPlugin, ScrollTrigger);
 onMounted(() => {
   const tl = gsap.timeline({
     scrollTrigger: {
-      trigger: '#name',
+      trigger: '#main',
       start: 'top top',
-      end: '500px',
+      end: 'center top',
       markers: false,
       scrub: true,
     },
   });
   tl.from('#line1', { ease: 'ease-in-out', drawSVG: 0, duration: 3 });
-  tl.to(
-    '.birth',
-    { ease: 'ease-in-out', opacity: 1, paddingLeft: '8px', duration: 6 },
-    '<0.5'
-  );
-  tl.from(
-    '#line2',
-    { ease: 'ease-in-out', drawSVG: '50% 50%', duration: 1 },
-    '<2.5'
-  );
+  tl.to('.birth', {
+    ease: 'ease-in-out',
+    opacity: 1,
+    paddingLeft: '8px',
+    duration: 6,
+  });
+  tl.from('#line2', { ease: 'ease-in-out', drawSVG: '50% 50%', duration: 1 });
   tl.from('#line3', { ease: 'ease-in-out', drawSVG: 0, duration: 3 });
   tl.to(
     '.something',
@@ -41,31 +38,20 @@ onMounted(() => {
 
 <template>
   <div
-    class="bio-body min-h-screen z-10 relative bg-[#fffef2] flex flex-col items-center"
+    id="main"
+    class="flex flex-col items-center min-h-[calc(100vh-4rem)] relative z-10 bg-white"
   >
-    <div
-      class="top-text min-h-[50vh] w-screen flex justify-center items-center bg-[#fdf8ec]"
-    >
-      <div
-        class="font-acier-bat-gris text-2xl md:text-9xl text-[#d6d3cb] flex flex-col items-center"
-      >
-        <div id="name"><span class="text-[#0c0e12]">Jered</span> Davis</div>
-        <div>Leisey</div>
-      </div>
-    </div>
-    <span class="font-hwt-aetna-streamer-outline text text-8xl"> *1986 </span>
+    <div class="text-8xl">1986</div>
     <div class="grid grid-cols-[1fr,max-content,1fr] justify-items-center">
       <svg id="chart" width="20" height="225" class="col-start-2">
         <line id="line1" x1="10" y1="0" x2="10" y2="224"></line>
         <line id="line2" x1="0" y1="224" x2="20" y2="224"></line>
       </svg>
       <div class="birth pt-12 col-start-3 opacity-0 left-1/2 pl-4 absolute">
-        *Born July 6th in Syracuse, NY.
+        Born July 6th in Syracuse, NY.
       </div>
     </div>
-    <span class="font-hwt-aetna-streamer-outline text text-8xl">
-      2009-2014
-    </span>
+    <div class="text-8xl">2014</div>
     <div class="grid grid-cols-[1fr,max-content,1fr] justify-items-center">
       <svg id="chart" width="20" height="225" class="col-start-2">
         <line id="line3" x1="10" y1="0" x2="10" y2="224"></line>
@@ -75,7 +61,7 @@ onMounted(() => {
         Attended UNCA for physics.
       </div>
     </div>
-    <span class="font-hwt-aetna-streamer-outline text text-8xl"> 2016 </span>
+    <div class="text-8xl">2023</div>
   </div>
 </template>
 
